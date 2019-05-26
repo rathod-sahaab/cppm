6914e0b5fb6abc3276ea74ac40e7dbb4541bedf8
Abhay Raj Singh <abhayonlyone@gmail.com>
2019-05-26 11:27:08 +0530

    added fill featur reorganised

--- include/ppm.hpp
+++ include/ppm.hpp
@@ -15,6 +15,7 @@ struct Header {
   friend std::ostream& operator<<(std::ostream&, const Header&);
 };
 
+// Base class PixMap
 template <class T>
 class PixMap {
   Header head;
@@ -30,38 +31,10 @@ class PixMap {
   inline unsigned get_height() const;
   inline unsigned get_width() const;
 
-  void set_format();
-  void set_width();
-  void set_height();
-  void set_max();
+  void fill(T val);
 
   PixMap<T>& set_pixel(size_t x, size_t y, T val);
 };
-class PixBitMap : public PixMap<unsigned> {
- public:
-  PixBitMap();
-  PixBitMap(size_t width, size_t height);
-  /* PixBitMap(unsigned, unsigned, std::string); */
-};
-
-class PixGreyMap : public PixMap<unsigned> {
- public:
-  PixGreyMap();
-  PixGreyMap(size_t width, size_t height, unsigned max_pixel_value);
-};
-
-struct RGB {
-  unsigned r, g, b;
-  friend std::ostream& operator<<(std::ostream&, const RGB&);
-  RGB();
-  RGB(unsigned, unsigned, unsigned);
-};
-class PixPixMap : public PixMap<RGB> {
- public:
-  PixPixMap();
-  PixPixMap(size_t width, size_t height, unsigned max_pixel_value);
-};
-
 std::ostream& operator<<(std::ostream& out, const Header& h) {
   out << h.format << '\n'
       << h.width << ' ' << h.height << '\n'
@@ -77,11 +50,6 @@ inline unsigned PixMap<T>::get_height() const {
 template <class T>
 inline unsigned PixMap<T>::get_width() const {
   return head.width;
-}
-
-std::ostream& operator<<(std::ostream& out, const RGB& rgb) {
-  out << rgb.r << ' ' << rgb.g << ' ' << rgb.b;
-  return out;
 }
 
 template <class U>
@@ -113,28 +81,67 @@ template <class T>
 PixMap<T>& PixMap<T>::set_pixel(size_t x, size_t y, T val) {
   if (x >= 0 and x < head.width and y >= 0 and y < head.height) {
     data.at(y).at(x) = val;
-  } else {
-    std::cerr << "Out of Bound access!\n";
   }
   return *this;
 }
 
+template <class T>
+void PixMap<T>::fill(T val) {
+  for (auto& row : data) {
+    for (auto& elm : row) {
+      elm = val;
+    }
+  }
+}
+// Base class PixMap
+
+class PixBitMap : public PixMap<unsigned> {
+ public:
+  PixBitMap();
+  PixBitMap(size_t width, size_t height);
+};
+
 PixBitMap::PixBitMap(size_t width, size_t height)
     : PixMap<unsigned>("P1", width, height, 1) {}
 
+class PixGreyMap : public PixMap<unsigned> {
+ public:
+  PixGreyMap();
+  PixGreyMap(size_t width, size_t height, unsigned max_pixel_value);
+};
+
 PixGreyMap::PixGreyMap(size_t width, size_t height, unsigned max_pixel_value)
     : PixMap<unsigned>("P2", width, height, max_pixel_value) {}
+
+struct RGB {
+  unsigned r, g, b;
+  friend std::ostream& operator<<(std::ostream&, const RGB&);
+  RGB();
+  RGB(unsigned, unsigned, unsigned);
+};
 
 RGB::RGB() {
   r = 0;
   g = 0;
   b = 0;
 }
+
 RGB::RGB(unsigned red, unsigned green, unsigned blue) {
   r = red;
   g = green;
   b = blue;
 }
+
+std::ostream& operator<<(std::ostream& out, const RGB& rgb) {
+  out << rgb.r << ' ' << rgb.g << ' ' << rgb.b;
+  return out;
+}
+
+class PixPixMap : public PixMap<RGB> {
+ public:
+  PixPixMap();
+  PixPixMap(size_t width, size_t height, unsigned max_pixel_value);
+};
 
 PixPixMap::PixPixMap(size_t width, size_t height, unsigned max_pixel_value)
     : PixMap<RGB>("P3", width, height, max_pixel_value) {}
